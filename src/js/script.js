cc27eae4656ac455f3f8e6d58dbbd40cdddab30a
Валерий Кузьменко <valerijkuzmenko@MacBook-Air-Valerij.local>
2023-04-26 22:08:50 +0300

    Test-Slide

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+function slider() {
+	let offset = 0; // снедение от левого кpan
+
+	const sliderLine = document.querySelector('.slider_line');
+
+	const sliderContainerWidth = document.querySelector('.slider').offsetWidth;
+
+	const maxWidth = sliderContainerWidth * 2;
+
+	document.querySelector('.next').addEventListener('click', function () {
+		offset = offset + sliderContainerWidth;
+
+		if (offset > maxWidth) {
+			offset = 0;
+		}
+		sliderLine.style.left = -offset + 'px';
+	});
+
+	document.querySelector('.prev').addEventListener('click', function () {
+
+		offset = offset - sliderContainerWidth;
+
+		if (offset < 0) {
+			offset = maxWidth;
+		}
+
+		sliderLine.style.left = -offset + 'px';
+	});
+};
+slider();
