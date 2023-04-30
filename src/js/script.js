428df2b53c36603a40fa399b7c4e5655484a7c2f
Валерий Кузьменко <valerijkuzmenko@MacBook-Air-Valerij.local>
2023-04-30 13:16:23 +0300

    TestSliderNew

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,88 @@
 function slider() {
-	let offset = 0; // снедение от левого кpan
+    let offset = 0; // снедение от левого кpan
+    let numberImage = 0;
 
-	const sliderLine = document.querySelector('.slider_line');
+    const sliderLine = document.querySelector('.slider_line');
 
-	const sliderContainerWidth = document.querySelector('.slider').offsetWidth;
+    const sliderContainerWidth = document.querySelector('.slider').offsetWidth;
 
-	const maxWidth = sliderContainerWidth * 2;
+    let maxWidth = 0;
 
-	document.querySelector('.next').addEventListener('click', function () {
-		offset = offset + sliderContainerWidth;
+    const itemElements = document.getElementsByClassName("item")
+    const maxNumberImage = (document.getElementsByClassName("item").length - 1)
 
-		if (offset > maxWidth) {
-			offset = 0;
-		}
-		sliderLine.style.left = -offset + 'px';
-	});
+    // Default settings margins for the first element
+    const imgOffsetWidth = document.getElementsByClassName("item")[0].childNodes[0].offsetWidth;
+    const y = (sliderContainerWidth - imgOffsetWidth) / 2;
+    document.getElementsByClassName("item")[0].style.marginLeft = y + 'px';
+    document.getElementsByClassName("item")[0].style.marginRight = y + 'px';
 
-	document.querySelector('.prev').addEventListener('click', function () {
+    for (const item of itemElements) {
+        const image = item.childNodes[0];
+        maxWidth = maxWidth + image.offsetWidth;
+    }
+    document.querySelector('.next').addEventListener('click', function () {
+        const imgOffsetWidth = document.getElementsByClassName("item")[numberImage].childNodes[0].offsetWidth;
+        const y = (sliderContainerWidth - imgOffsetWidth) / 2;
 
-		offset = offset - sliderContainerWidth;
+        if (numberImage < maxNumberImage) {
+            document.getElementsByClassName("item")[numberImage + 1].style.marginLeft = y + 'px';
+            document.getElementsByClassName("item")[numberImage + 1].style.marginRight = y + 'px';
+        }
 
-		if (offset < 0) {
-			offset = maxWidth;
-		}
+        if (numberImage > 0) {
+            document.getElementsByClassName("item")[numberImage].style.marginLeft = 0 + 'px';
+            document.getElementsByClassName("item")[numberImage].style.marginRight = 0 + 'px';
+        }
+        if (numberImage === maxNumberImage) {
+            document.getElementsByClassName("item")[0].style.marginLeft = y + 'px';
+            document.getElementsByClassName("item")[0].style.marginRight = y + 'px';
+        } else {
+            document.getElementsByClassName("item")[0].style.marginLeft = 0 + 'px';
+            document.getElementsByClassName("item")[0].style.marginRight = 0 + 'px';
+        }
 
-		sliderLine.style.left = -offset + 'px';
-	});
+        offset = offset + (imgOffsetWidth);
+
+        if (offset >= maxWidth) {
+            offset = 0;
+            numberImage = 0;
+        } else {
+            numberImage++;
+        }
+        sliderLine.style.left = -(offset) + 'px';
+    });
+
+    document.querySelector('.prev').addEventListener('click', function () {
+        const imgOffsetWidth = document.getElementsByClassName("item")[numberImage].childNodes[0].offsetWidth;
+        const y = (sliderContainerWidth - imgOffsetWidth) / 2;
+
+        if (numberImage === 0) {
+            numberImage = maxNumberImage;
+            document.getElementsByClassName("item")[numberImage].style.marginLeft = y + 'px';
+            document.getElementsByClassName("item")[numberImage].style.marginRight = y + 'px';
+            document.getElementsByClassName("item")[0].style.marginLeft = 0 + 'px';
+            document.getElementsByClassName("item")[0].style.marginRight = 0 + 'px';
+        } else {
+            document.getElementsByClassName("item")[numberImage - 1].style.marginLeft = y + 'px';
+            document.getElementsByClassName("item")[numberImage - 1].style.marginRight = y + 'px';
+
+            document.getElementsByClassName("item")[numberImage].style.marginLeft = 0 + 'px';
+            document.getElementsByClassName("item")[numberImage].style.marginRight = 0 + 'px';
+        }
+
+        offset = offset - (imgOffsetWidth);
+
+        if (offset < 0) {
+            offset = maxWidth - imgOffsetWidth;
+            numberImage = 2;
+        } else {
+            if (numberImage > 0) {
+                numberImage--;
+            }
+        }
+
+        sliderLine.style.left = -offset + 'px';
+    });
 };
 slider();
