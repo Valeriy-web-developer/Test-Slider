2029d2312f63e13a0b843cafa8f44e5318d450f4
Валерий Кузьменко <valerijkuzmenko@MacBook-Air-Valerij.local>
2023-05-11 10:14:01 +0300

    TabsHappy

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,23 @@
-function slider(){
-let offset = 0; 
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
 
-const sliderLine = document.querySelector('.slider_line');
+for (let i = 0; i < tabs.length; i++) {
+	tabs[i].addEventListener( 'click', (event) => {
+		//удаляем класс
+		let tabsCurrent = event.target.parentElement.children;
+		for(let t = 0; t < tabsCurrent.length; t++) {
+			tabsCurrent[t].classList.remove('tab--active');
+		}
+		//добавляем класс текущий
+		event.target.classList.add('tab--active');
 
-const sliderContainerWidth = document.querySelector('.slider').offsetWidth;
-
-const maxWidth = sliderContainerWidth * 2;
-
-document.querySelector('.next').addEventListener('click', function(){ 
- offset = offset + sliderContainerWidth;
-
- if (offset > maxWidth) {
-   offset = 0;
-  }
-  sliderLine.style.left = -offset + 'px';
-})
-
-document.querySelector('.prev').addEventListener('click', function(){
-  
- offset = offset - sliderContainerWidth;
-
- if (offset < 0) {
-     offset = maxWidth;
-  }
-  sliderLine.style.left = -offset + 'px';
-})
+		//удаляем класс
+		let contentsCurrent = event.target.parentElement.nextElementSibling.children;
+		
+		for(let c = 0; c < contentsCurrent.length; c++) {
+			contentsCurrent[c].classList.remove('content--active');
+		}
+		//добавляем класс текущий
+		contents[i].classList.add('content--active');
+	});
 }
-slider();
